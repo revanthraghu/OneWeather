1a5868792e07dbe4d22b4d650770df80ad3c51a3
Revanth Raghu <revabob@gmail.com>
2020-08-12 18:30:14 +0530

    basic layout of current weather completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import CurrentWeather from './components/CurrentWeather'
 import data from './data/data.json'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const VideoContainer = styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%; 
+    overflow: hidden;
+    video {
+        min-width: 100%; 
+        min-height: 100%; 
+        width: auto;
+        height: auto;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+    }
+`
+
 class App extends React.Component {
   //console.log(process.env.REACT_APP_MY_SECRET_KEY)
   constructor(props) {
@@ -14,54 +35,63 @@ class App extends React.Component {
     }
   }
 
-  async componentDidMount() {
+  saveData = (data) => {
+    this.setState({data: data})
+  }
+
+  getPosition = (position) => {
+    //get weather daat from api
+    try {
+      axios({
+        method: 'GET',
+        baseURL: 'https://api.openweathermap.org/data/2.5',
+        url: '/onecall',
+        params: { 
+          lat: position.coords.latitude, 
+          lon: position.coords.longitude,
+          exclude: 'minutely',
+          units: 'metric',
+          appid: process.env.REACT_APP_MY_SECRET_KEY
+        }
+      })
+      .then(res => this.setState({
+        data: res.data, 
+        lat: position.coords.latitude, 
+        lon: position.coords.longitude
+      }))
+    } catch (error) {
+      console.log(error)
+    }
+      //temporarily using saved data to avoid extra api calls
+      //this.setState({data: data, lat: position.coords.latitude, lon: position.coords.longitude})
+  }
+
+  componentDidMount() {
     //Ask for location permission
     //If location access denied show alert to say access required
-    //If access given set lat, lon state and get data
-    // try {
-    //   await axios({
-    //     method: 'GET',
-    //     baseURL: 'https://api.openweathermap.org/data/2.5',
-    //     url: '/onecall',
-    //     params: { 
-    //       lat: this.state.lat, 
-    //       lon: this.state.lon,
-    //       exclude: 'minutely',
-    //       units: 'metric',
-    //       appid: process.env.REACT_APP_MY_SECRET_KEY
-    //     }
-    //   }).then(res => this.setState({data: res.data}))
-    // } catch (error) {
-    //   console.log(error)
-    // }
-    this.setState({data: data})
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getPosition);
+    }
   }
 
   render() {
     if(Object.keys(this.state.data).length === 0) {
       return (
-        <div style={{width: '100vw', height: '100vh', backgroundColor: '#311B92'}}>
-
-        </div>
+        <div style={{width: '100vw', height: '100vh', backgroundColor: '#311B92'}}></div>
       )
     }
     else {
       return (
-        <CurrentWeather weather={this.state.data.current}/>
+        <React.Fragment>
+          <VideoContainer>
+            <video autoPlay muted loop>
+                <source src='thunderstorm.mp4' type='video/mp4'/> 
+            </video>
+          </VideoContainer>
+          <CurrentWeather saveData={this.saveData} weather={this.state.data.current} temps={[this.state.data.daily[0].temp.min, this.state.data.daily[0].temp.max]}/>
+        </React.Fragment>
       );
     }
   }
-=======
-import Landing from './Components/Landing';
-
-function App() {
-  // console.log(process.env.REACT_APP_MY_SECRET_KEY)
-  return (
-    <div>
-      <Landing />
-    </div>
-  );
->>>>>>> ef9653586d4b5aa58bca78752569d2ff645a85ea
 }
-
 export default App;

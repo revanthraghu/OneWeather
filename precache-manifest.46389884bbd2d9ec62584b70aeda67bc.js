085c43259471d9320644f40d2509ee68926f31fb
Revanth Raghu <revabob@gmail.com>
2020-08-19 13:21:58 +0530

    Updates

---
+++ precache-manifest.46389884bbd2d9ec62584b70aeda67bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f047828b94806675441c50ec698eeb",
+    "url": "/one-weather/index.html"
+  },
+  {
+    "revision": "3f7b844f0c527935c0b9",
+    "url": "/one-weather/static/js/2.8a67c78b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/one-weather/static/js/2.8a67c78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c0b5e6c610f5c4ac2f",
+    "url": "/one-weather/static/js/main.4b0a9dee.chunk.js"
+  },
+  {
+    "revision": "3810a991bd0290a32769",
+    "url": "/one-weather/static/js/runtime-main.c5f8b6f0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/one-weather/precache-manifest.46389884bbd2d9ec62584b70aeda67bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/one-weather/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
